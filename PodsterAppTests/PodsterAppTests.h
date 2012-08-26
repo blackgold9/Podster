c166d7d0f4658d94b896caa5badd08f454f39b91
Stephen Vanterpool <stephen@vanterpool.net>
2012-08-25 23:56:57 -0700

    Download fix

---
+++ PodsterAppTests/PodsterAppTests.h
@@ -0,0 +1,13 @@
+//
+//  PodsterAppTests.h
+//  PodsterAppTests
+//
+//  Created by Stephen J Vanterpool on 8/25/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PodsterAppTests : SenTestCase
+
+@end
---
+++ PodsterAppTests/PodsterAppTests.m
@@ -0,0 +1,32 @@
+//
+//  PodsterAppTests.m
+//  PodsterAppTests
+//
+//  Created by Stephen J Vanterpool on 8/25/12.
+//
+//
+
+#import "PodsterAppTests.h"
+
+@implementation PodsterAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PodsterAppTests");
+}
+
+@end
